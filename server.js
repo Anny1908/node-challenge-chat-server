a6f5cc305fc528cf27c66fbe808a275e45ca4fe3
Anny <annylianmarian@gmail.com>
2020-11-23 16:56:07 +0100

    Nivel 1

--- server.js
+++ server.js
@@ -1,23 +1,52 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require('fs');
+const file = require("./file.json");
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(cors());
-
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
-};
+app.use(bodyParser.json());
 
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get("/", function(request, response) {
+    response.send("welcome Chat");
 });
 
-app.listen(process.env.PORT);
+
+app.get("/file", function(request, response) {
+    response.send(file);
+});
+
+app.post("/file/add", (req, res) => {
+    const message = req.body
+    message.id = file[file.length - 1].id + 1;
+    console.log("esto", file.length);
+    const chats = file;
+    chats.push(message);
+    fs.writeFileSync("./file.json", JSON.stringify(chats), () => {});
+    res.send(message);
+});
+
+app.delete("/file/:id", (req, res) => {
+    const id = req.params.id;
+    const chat = file.filter(el => el.id === Number(id));
+    let chats = file;
+    let index = chats.indexOf(chat[0]);
+    chats.splice(index, 1);
+    fs.writeFileSync("./file.json", JSON.stringify(chats), () => {});
+    res.status(200).json({ succes: true });
+})
+
+app.get("/file/:id", (req, res) => {
+    const id = req.params.id;
+    const chat = file.filter(el => el.id === Number(id));
+    res.send(chat);
+})
+app.listen(4001, function() {
+    console.log("Your app is listening on port 4001");
+});
