07f2bd6f8d0c9635087f954dcbc2232fff880f92
Anny <annylianmarian@gmail.com>
2020-11-25 16:05:14 +0100

    nivel 3 y 4

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+const newChats = file;
+
+
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
@@ -17,36 +20,55 @@ app.get("/", function(request, response) {
     response.send("welcome Chat");
 });
 
-
-app.get("/file", function(request, response) {
-    response.send(file);
+app.get("/messages", function(request, response) {
+    response.send(newChats);
 });
 
-app.post("/file/add", (req, res) => {
+app.get("/messages/search", (req, res) => {
+    let texto = req.query.text;
+    let search = newChats.find(element => element.text.includes(texto));
+    console.log(newChats);
+    res.send(search);
+});
+
+app.get("/messages/latest", (req, res) => {
+    let ultimos = newChats.slice(-10);
+    res.send(ultimos);
+})
+
+app.get("/messages/:id", (req, res) => {
+    const id = req.params.id;
+    const chat = newChats.find(el => el.id === Number(id));
+    console.log("id", id);
+    res.send(chat);
+})
+app.post("/messages", (req, res) => {
     const message = req.body
-    message.id = file[file.length - 1].id + 1;
-    console.log("esto", file.length);
-    const chats = file;
-    chats.push(message);
-    fs.writeFileSync("./file.json", JSON.stringify(chats), () => {});
+    message.id = newChats[newChats.length - 1].id + 1;
+    console.log("esto", newChats.length);
+    if (message.from == "" || message.from == undefined) {
+        res.sendStatus(400);
+    } else if (message.text == "" || message.text == undefined) {
+        res.sendStatus(400);
+    } else {
+        newChats.push(message);
+        message.timeSent = new Date();
+    }
+    fs.writeFileSync("./file.json", JSON.stringify(newChats), () => {});
     res.send(message);
 });
 
-app.delete("/file/:id", (req, res) => {
+app.delete("/messages/:id", (req, res) => {
     const id = req.params.id;
     const chat = file.filter(el => el.id === Number(id));
-    let chats = file;
-    let index = chats.indexOf(chat[0]);
-    chats.splice(index, 1);
-    fs.writeFileSync("./file.json", JSON.stringify(chats), () => {});
+    let newChats = file;
+    let index = newChats.indexOf(chat[0]);
+    newChats.splice(index, 1);
+    fs.writeFileSync("./file.json", JSON.stringify(newChats), () => {});
     res.status(200).json({ succes: true });
 })
 
-app.get("/file/:id", (req, res) => {
-    const id = req.params.id;
-    const chat = file.filter(el => el.id === Number(id));
-    res.send(chat);
-})
+
 app.listen(4001, function() {
     console.log("Your app is listening on port 4001");
 });
